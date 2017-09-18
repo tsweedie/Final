19e8983a86d59dc087c72df2ee7388fd88faa60d
tsweedie <3342317@myuwc.ac.za>
2017-09-18 18:31:01 +0200

    final features*

---
+++ modelData.py
@@ -0,0 +1,192 @@
+import cv2
+import os
+import shutil
+from extractFeatures import FeatureExtraction
+import csv
+import xlsxwriter
+
+
+class MineData():
+    def load_images_from_folder(self, folder):
+        print 'Load images from CK+', folder
+        images = []
+        for filename in os.listdir(folder):
+            if filename.endswith('.png'):
+                img = cv2.imread(os.path.join(folder, filename))
+                if img is not None:
+                    images.append(img)
+        return images
+
+    def main(self):
+        print 'Starting...'
+        self.features = FeatureExtraction()
+
+        pathAngry = os.path.abspath('Data/Angry')
+        pathDisgust = os.path.abspath('Data/Disgust')
+        pathFear = os.path.abspath('Data/Fear')
+        pathHappy = os.path.abspath('Data/Happy')
+        pathNeutral = os.path.abspath('Data/Neutral') 
+        pathSad = os.path.abspath('Data/Sad')
+        pathSurprise = os.path.abspath('Data/Surprise')
+
+        Angry = self.load_images_from_folder(pathAngry)
+        Disgust = self.load_images_from_folder(pathDisgust)
+        Fear = self.load_images_from_folder(pathFear)
+        Happy = self.load_images_from_folder(pathHappy)
+        Neutral = self.load_images_from_folder(pathNeutral)
+        Sad = self.load_images_from_folder(pathSad)
+        Surprise = self.load_images_from_folder(pathSurprise)
+
+        current_directory = os.getcwd()
+        shutil.rmtree(current_directory + '/Data/Viola')
+
+        if not os.path.exists(os.path.join(current_directory, r'Data/Viola/angViola')):
+            os.makedirs(os.path.join(current_directory, r'Data/Viola/angViola'))
+        if not os.path.exists(os.path.join(current_directory, r'Data/Viola/disViola')):
+            os.makedirs(os.path.join(current_directory, r'Data/Viola/disViola'))
+        if not os.path.exists(os.path.join(current_directory, r'Data/Viola/fearViola')):
+            os.makedirs(os.path.join(current_directory, r'Data/Viola/fearViola'))
+        if not os.path.exists(os.path.join(current_directory, r'Data/Viola/hapViola')):
+            os.makedirs(os.path.join(current_directory, r'Data/Viola/hapViola'))
+        if not os.path.exists(os.path.join(current_directory, r'Data/Viola/neuViola')):
+            os.makedirs(os.path.join(current_directory, r'Data/Viola/neuViola'))
+        if not os.path.exists(os.path.join(current_directory, r'Data/Viola/sadViola')):
+            os.makedirs(os.path.join(current_directory, r'Data/Viola/sadViola'))
+        if not os.path.exists(os.path.join(current_directory, r'Data/Viola/surViola')):
+            os.makedirs(os.path.join(current_directory, r'Data/Viola/surViola'))
+
+        print 'Running Viola-Jones on CK+ Images'
+        angViola, disViola, fearViola, hapViola, neuViola, sadViola, surViola = [], [], [], [], [], [], []
+        for i in range(0, 84):
+            if i < len(Angry):
+                v1, image = self.features.viola_jones(Angry[i])
+                angViola.append(v1)
+                cv2.imwrite(current_directory + '/Data/Viola/angViola/angry' + str(i) + '.png', v1)
+            if i < len(Disgust):
+                v2, image = self.features.viola_jones(Disgust[i])
+                disViola.append(v2)
+                cv2.imwrite(current_directory + '/Data/Viola/disViola/disgust' + str(i) + '.png', v2)
+            if i < len(Fear):
+                v3, image = self.features.viola_jones(Fear[i])
+                fearViola.append(v3)
+                cv2.imwrite(current_directory + '/Data/Viola/fearViola/fear' + str(i) + '.png', v3)
+            if i < len(Happy):
+                v4, image = self.features.viola_jones(Happy[i])
+                hapViola.append(v4)
+                cv2.imwrite(current_directory + '/Data/Viola/hapViola/happy' + str(i) + '.png', v4)
+            if i < len(Neutral):
+                v5, image = self.features.viola_jones(Neutral[i])
+                neuViola.append(v5)
+                cv2.imwrite(current_directory + '/Data/Viola/neuViola/neutral' + str(i) + '.png', v5)
+            if i < len(Sad):
+                v6, image = self.features.viola_jones(Sad[i])
+                sadViola.append(v6)
+                cv2.imwrite(current_directory + '/Data/Viola/sadViola/sad' + str(i) + '.png', v6)
+            if i < len(Surprise):
+                v7, image = self.features.viola_jones(Surprise[i])
+                surViola.append(v7)
+                cv2.imwrite(current_directory + '/Data/Viola/surViola/surprise' + str(i) + '.png', v7)
+                
+        print 'Running Hog on Viola-Jones Images'
+        featureVectors = []
+        for i in range(0, 84):
+            if i < len(angViola):
+                angVector = self.features.hog_opencv(angViola[i])
+                angVector = angVector.tolist()
+                angVector.insert(0, 1)
+                featureVectors.append(angVector)
+            if i < len(disViola):
+                disVector = self.features.hog_opencv(disViola[i])
+                disVector = disVector.tolist()
+                disVector.insert(0, 2)
+                featureVectors.append(disVector)
+            if i < len(fearViola):
+                fearVector = self.features.hog_opencv(fearViola[i])
+                fearVector = fearVector.tolist()
+                fearVector.insert(0, 3)
+                featureVectors.append(fearVector)
+            if i < len(hapViola):
+                hapVector = self.features.hog_opencv(hapViola[i])
+                hapVector = hapVector.tolist()
+                hapVector.insert(0, 4)
+                featureVectors.append(hapVector)
+            if i < len(neuViola):
+                neuVector = self.features.hog_opencv(neuViola[i])
+                neuVector = neuVector.tolist()
+                neuVector.insert(0, 5)
+                featureVectors.append(neuVector)
+            if i < len(sadViola):
+                sadVector = self.features.hog_opencv(sadViola[i])
+                sadVector = sadVector.tolist()
+                sadVector.insert(0, 6)
+                featureVectors.append(sadVector)
+            if i < len(surViola):
+                surVector = self.features.hog_opencv(surViola[i])
+                surVector = surVector.tolist()
+                surVector.insert(0, 7)
+                featureVectors.append(surVector)
+                
+        with open('Files/dataCsv.csv', 'wb+') as f:
+            writer = csv.writer(f)
+            writer.writerows(featureVectors)
+            
+        workbook = xlsxwriter.Workbook('Files/dataExcel.xlsx')
+        worksheet = workbook.add_worksheet()
+        for row, vector in enumerate(featureVectors):
+            for col, value in enumerate(vector):
+                worksheet.write(row, col, value)
+        workbook.close()
+
+        print 'Running My Hog on Viola-Jones Images'
+        featureVectors = []
+        for i in range(0, 84):
+            if i < len(angViola):
+                angVector = self.features.calculate_myhog(angViola[i])
+                angVector = angVector.tolist()
+                angVector.insert(0, 1)
+                featureVectors.append(angVector)
+            if i < len(disViola):
+                disVector = self.features.calculate_myhog(disViola[i])
+                disVector = disVector.tolist()
+                disVector.insert(0, 2)
+                featureVectors.append(disVector)
+            if i < len(fearViola):
+                fearVector = self.features.calculate_myhog(fearViola[i])
+                fearVector = fearVector.tolist()
+                fearVector.insert(0, 3)
+                featureVectors.append(fearVector)
+            if i < len(hapViola):
+                hapVector = self.features.calculate_myhog(hapViola[i])
+                hapVector = hapVector.tolist()
+                hapVector.insert(0, 4)
+                featureVectors.append(hapVector)
+            if i < len(neuViola):
+                neuVector = self.features.calculate_myhog(neuViola[i])
+                neuVector = neuVector.tolist()
+                neuVector.insert(0, 5)
+                featureVectors.append(neuVector)
+            if i < len(sadViola):
+                sadVector = self.features.calculate_myhog(sadViola[i])
+                sadVector = sadVector.tolist()
+                sadVector.insert(0, 6)
+                featureVectors.append(sadVector)
+            if i < len(surViola):
+                surVector = self.features.calculate_myhog(surViola[i])
+                surVector = surVector.tolist()
+                surVector.insert(0, 7)
+                featureVectors.append(surVector)
+                
+        with open('Files/myHogDataCsv.csv', 'wb+') as f:
+            writer = csv.writer(f)
+            writer.writerows(featureVectors)
+            
+        workbook = xlsxwriter.Workbook('Files/myHogDataExcel.xlsx')
+        worksheet = workbook.add_worksheet()
+        for row, vector in enumerate(featureVectors):
+            for col, value in enumerate(vector):
+                worksheet.write(row, col, value)
+        workbook.close()
+        print 'Done ^_^'
+
+
+if __name__ == '__main__': MineData().main()
---
+++ traintestSVM.py
@@ -0,0 +1,79 @@
+import sys
+import pandas as pd
+from sklearn import svm
+from sklearn.model_selection import GridSearchCV, cross_val_score, StratifiedKFold, train_test_split
+from sklearn.metrics import confusion_matrix, accuracy_score, classification_report
+import pickle
+
+
+class Tester():
+    def run_test(self, data, modelname):
+        print '\n######################################################################'
+        print '##########################~', modelname, '~#####################'
+        print '######################################################################'
+
+        dataset = pd.read_csv(data)
+        X = dataset.ix[:, 1:].values
+        y = dataset.ix[:, 0].values
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.40, stratify=y, random_state=40)
+
+        # C = np.logspace(-5, 15,num=21,base = 2.0)
+        # gamma = np.logspace(-15, 3, num=19,base = 2.0)
+        param_grid = [
+            {'C': [1, 10, 100, 1000], 'kernel': ['linear']},
+            {'C': [1, 10, 100, 1000], 'gamma': [0.001, 0.0001], 'kernel': ['rbf']},
+        ]
+        svc = svm.SVC(probability=True)
+
+        print '\n######################################################################'
+        print '#############################~SVM Grid Search~########################'
+        print '######################################################################'
+        clf = GridSearchCV(svc, param_grid)
+        print '\t Parameter Grid:\n', param_grid
+
+        print '\n######################################################################'
+        print '##########################~SVM Cross Validation~######################'
+        print '######################################################################'
+        skf = StratifiedKFold(n_splits=3, random_state=None, shuffle=False)
+        scores = cross_val_score(clf, X, y, cv=skf, n_jobs=-1)
+        print '\t Cross validation scores: \t', scores
+        print '\t Cross validation Accuracy:\t %0.2f (+/- %0.2f)' % (scores.mean(), scores.std() * 2)
+
+        print '\n######################################################################'
+        print '#############################~SVM Train~##############################'
+        print '######################################################################'
+        clf = clf.fit(X_train, y_train)
+        filename = modelname
+        pickle.dump(clf, open('Files/'+filename, 'wb'))
+        print '\t Best score for classifier:\t', clf.best_score_
+        print '\t Best C:\t', clf.best_estimator_.C
+        print '\t Best Kernel:\t', clf.best_estimator_.kernel
+        print '\t Best Gamma:\t', clf.best_estimator_.gamma
+        print '\t SVM Best Estimator:\t', clf.best_estimator_
+        print '\n\t SVM Grid Scores: \n', clf.cv_results_
+
+        print '\n######################################################################'
+        print '#############################~SVM Predict~############################'
+        print '######################################################################'
+        clf = pickle.load(open('Files/'+modelname, 'rb'))
+        y_pred = clf.predict(X_test)
+        print 'SVM Classification Report:'
+        print classification_report(y_test, y_pred,
+                                    target_names=['Angry', 'Disgust', 'Fear', 'Happy', 'Neutral', 'Sad', 'Surprise'])
+        print 'SVM Confusion Matrix:'
+        print confusion_matrix(y_test, y_pred, labels=range(7))
+        print '\t SVM Accuracy Score:', accuracy_score(y_test, y_pred, normalize=True)
+        print 'file:', data
+
+
+    def main(self):
+        print 'Output is printed to Files/test.out'
+        orig_stdout = sys.stdout
+        output = open('Files/test.out', 'w+')
+        sys.stdout = output
+        self.run_test('Files/dataCsv.csv', 'finalized_model.sav')
+        self.run_test('Files/myHogDataCsv.csv', 'myHogfinalized_model.sav')
+        sys.stdout.close()
+        sys.stdout = orig_stdout
+        print 'Done ^_^'
+if __name__ == '__main__': Tester().main()
